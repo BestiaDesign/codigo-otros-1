a8b6462ae43be48de5217f8d5ab89fdd84c2da35
Johanna Villegas <110148988+BestiaDesign@users.noreply.github.com>
2023-01-20 19:08:24 -0600

    Se añadió el código anterior para comparar.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,33 @@
+// Código anterior
+/*
+
+const baseEndpoint = 'https://api.github.com';
+const usersEndpoint = `${baseEndpoint}/users`;
+const $n = document.querySelector('name');
+const $b = document.querySelector('#blog');
+const $l = document.querySelector('.location');
+
+function displayUser(username) {
+  $n.textContent = 'cargando...';
+  const response = await fetch(`${usersEndpoint}/${username}`);
+  console.log(data);
+  $n.textContent = '${data.name}';
+  $b.textContent = '${data.blog}';
+  $l.textContent = '${data.location}';
+}
+
+function handleError(err) {
+  console.log('OH NO!');
+  console.log(err);
+  n.textContent = `Algo salió mal: ${err}`
+}
+
+displayUser('stolinski').catch(handleError);
+
+*/
+
+// Mi intento
+
 const baseEndpoint = "https://api.github.com"; // Se cambio a comillas dobles
 const usersEndpoint = `${baseEndpoint}/users`;
 const $n = document.querySelector('.name'); // Se agrego el . para llamar a la clase
